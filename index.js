0f3d5192d35d227cc747782759118d53b2bbbfe7
ericscamander <eric.lopez@duocuc.cl>
2023-05-18 19:25:26 -0400

    Cambio proyecto completo

---
+++ index.js
@@ -0,0 +1,25 @@
+const API_URL = 'https://jsonplaceholder.typicode.com/users';
+const HTMLResponse = document.querySelector("#app");
+const ul = document.createElement("ul");
+
+
+fetch(`${API_URL}/users`)
+    .then((response) => response.json())
+    .then((users) => {
+        users.array.forEach(element => {
+            
+        });((user) =>{
+        let elem = document.createElement("li");
+        elem.appendChild(
+            document.createTextNode(`${user.name} ✉ ${user.email}`));
+        ul.appendChild(elem);
+    
+    });
+
+        HTMLResponse.appendChild(ul);
+
+
+    });
+
+    
+        
